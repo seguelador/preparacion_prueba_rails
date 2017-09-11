584b69f0ccdae2c76b0237b6401e7270d8d7a4bb
Matias Seguel Estay <matias.seguel.estay@gmail.com>
2017-09-10 22:37:33 -0300

    Se agrega dema devise para crear modelo de User y se agrega un enum con roles del usuario

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: {guest: 0, basic: 1, admin: 2}
+end
