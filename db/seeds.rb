34dac6884ec44f3de74422dedacf66a49004b3ba
Matias Seguel Estay <matias.seguel.estay@gmail.com>
2017-09-10 22:24:25 -0300

    Creado el recurso Pokemon utilizando scaffold y se crea seed con 5 pokemons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pokemon.destroy_all
+
+Pokemon.create(name: 'Pikachu', description: 'Pokemon ratón y que paza')
+Pokemon.create(name: 'Bulbasaur', description: 'Soy una planta')
+Pokemon.create(name: 'Charmander', description: 'Si se me apaga la llama de la cola, muero :(')
+Pokemon.create(name: 'Ho Ho', description: 'Nadie me conoce')
+Pokemon.create(name: 'Squartle', description: 'Tortuga que tira agua')
